5b4998314ba5939908ee7d2ece3c5d3ea479f317
Mauro Monteiro Silva <maumontesilva@gmail.com>
2018-05-02 23:22:20 +0100

    subscriber receiving message over TLS

--- src/iot-watchdog-consumer.py
+++ src/iot-watchdog-consumer.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import ssl
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -12,6 +13,8 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("test.mosquitto.org", 1883, 60)
+client.tls_set("server.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
+
+client.connect("192.168.56.101", 8883, 60)
 
 client.loop_forever()
